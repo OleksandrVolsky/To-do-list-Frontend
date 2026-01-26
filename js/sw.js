da3dc913db00fd4a11766a43c2729737a8c34f28
Oleksandr Volskyi <136353538+OleksandrVolsky@users.noreply.github.com>
2026-01-26 18:53:20 +0200

    update

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,8 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener('install', (event) => {
+  console.log('Service Worker встановлено');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Необов’язково кешувати
+  console.log('Service Worker fetch:', event.request.url);
+});
